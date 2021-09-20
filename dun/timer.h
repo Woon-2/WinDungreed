dd9657b9edae4cdacad23a3a9633155a03b21f14
MyeongWoonJang <73771162+MyeongWoonJang@users.noreply.github.com>
2021-09-21 06:25:02 +0900

    timer.h 프레임 가변화, worldtime 추가
    프레임 당 시간은 상수가 아니라 변수가 되었음.
    
    따라서, 프레임 처리가 아니라 실제 시간 기준 처리가 필요한 경우 worldtime을 이용함

--- dun/timer.h
+++ dun/timer.h
@@ -11,13 +11,85 @@
 #include "keyword.h"
 #include "TMP.h"
 #include <algorithm>
+#include <queue>
+#include <numeric>
+#include <deque>
+#include <functional>
+#include <utility>
 
-class timer NONCREATABLE
+class timer
 {
 public:
-	static constexpr int ms_per_frame() PURE
+	// for not frame-based timer uses
+	class request
 	{
-		return 30;
+	public:
+		// after ms_time, wrapped_func will be executed.
+		// wrap your func in a lambda expression.
+		// [ &args... ]() { your_static_func( args... ) }
+		// [ this, &args... ]() { this-> your_class_func( args... ) }
+		template < typename Func >
+		explicit request( const long long ms_time, Func wrapped_func ) :
+			_world_time_expected{ ms_time }, wrapped_func{ wrapped_func } {}
+
+		request() = delete;
+
+		request( const request& other ) : _world_time_expected{ other._world_time_expected },
+			wrapped_func{ other.wrapped_func } {}
+
+		request& operator=( const request& other )
+		{
+			if ( this != &other )
+			{
+				_world_time_expected = other._world_time_expected;
+				wrapped_func = other.wrapped_func;
+			}
+
+			return *this;
+		}
+
+		request( request&& other ) noexcept : _world_time_expected{ other._world_time_expected },
+			wrapped_func{ std::move( other.wrapped_func ) } {}
+
+		request& operator=( request&& other ) noexcept
+		{
+			if ( this != &other )
+			{
+				_world_time_expected = other._world_time_expected;
+				wrapped_func = std::move( other.wrapped_func );
+			}
+
+			return *this;
+		}
+
+		const bool operator>( const request& other ) const noexcept
+		{
+			return _world_time_expected > other._world_time_expected;
+		}
+
+		const long long world_time_expected() const noexcept
+		{
+			return _world_time_expected;
+		}
+
+		void handle() const
+		{
+			wrapped_func();
+		}
+
+	private:
+		long long _world_time_expected;
+		std::function< void() > wrapped_func;
+	};
+
+	static constexpr void setFPS( const int fps ) noexcept
+	{
+		_ms_per_frame = 1000 / fps;
+	}
+
+	static constexpr const int ms_per_frame() noexcept
+	{
+		return _ms_per_frame;
 	}
 
 	static void on_timer()
@@ -31,26 +103,76 @@ public:
 	timer() = delete;
 
 private:
-	HELPER static inline void go_routines( const int current_lag )
+	static void go_routines( const int current_lag )
 	{
 		update();
 		if ( current_lag < ms_per_frame() )
 			render();
 	}
 
-	HELPER static void update()
+	static void update()
+	{
+		world_time_based_update();
+		frame_based_update();
+	}
+
+	static void frame_based_update()
 	{
 		std::cout << "업데이트 루틴!\n";
 		for ( int i = 0; i < 20000000; ++i )
+		{
 			i += 2;
+		}
 	}
 
-	HELPER static void render()
+	static void world_time_based_update()
+	{
+		update_world_time();
+
+		while ( should_handle_a_request() )
+		{
+			handle_a_request();
+		}
+	}
+
+	static void update_world_time()
+	{
+		using namespace std::chrono;
+		static auto last_time = system_clock::now();
+		world_time += duration_cast< milliseconds >( last_time - system_clock::now() ).count();
+	}
+
+	static const bool should_handle_a_request()
+	{
+		if ( requests.empty() )
+		{
+			return false;
+		}
+		return requests.top().world_time_expected() > world_time;
+	}
+
+	static void handle_a_request()
+	{
+		requests.top().handle();
+		requests.pop();
+	}
+
+	static void render()
 	{
 		std::cout << "렌더 루틴!\n";
 		for ( int i = 0; i < 40000000; ++i )
+		{
 			i += 2;
+		}
 	}
+
+	static int _ms_per_frame;
+	static long long world_time;
+	static std::priority_queue< request, std::deque< request >, std::greater< request > > requests;
 };
+
+int timer::_ms_per_frame = 1000 / 30;
+long long timer::world_time = 0;
+std::priority_queue< timer::request, std::deque< timer::request >, std::greater< timer::request > > timer::requests;
 
 #endif
